fee374f2410240912544f1c5d3a44ac7a7126f51
Jordan Huus <jordanhuusi@gmail.com>
2020-05-31 16:40:09 -0700

    Successfully created first piece object from the Piece class.

--- backend/model/generate_training_data.py
+++ backend/model/generate_training_data.py
@@ -5,26 +5,32 @@ import math
 class Piece:
 
     pieces_count = 0
+    TOP = "top"
+    BOTTOM = "bottom"
+    LEFT = "left"
+    RIGHT = "right"
+
 
     def __init__(self, location, piece_size, initiating_piece, incoming_side, side_data, image_object):
 
-        
+
         Piece.pieces_count += 1
 
-        connected_pieces = []
+        self.connected_pieces = []
 
         # Side data
-        self.right_side = (location[0]+piece_size[0], location[1], location[0]+piece_size[0], location[1]+piece_size[1])
-        self.left_side = (location[0], location[1], location[0], location[1]+piece_size[1])
-        self.bottom_side = (location[0], location[1]+piece_size[1], location[0]+piece_size[0], location[1]+piece_size[1])
-        self.top_side = (location[0], location[1], location[0]+piece_size[0], location[1])
+        self.right_side = [(location[0]+piece_size[0], location[1], location[0]+piece_size[0], location[1]+piece_size[1])]
+        self.left_side = [(location[0], location[1], location[0], location[1]+piece_size[1])]
+        self.bottom_side = [(location[0], location[1]+piece_size[1], location[0]+piece_size[0], location[1]+piece_size[1])]
+        self.top_side = [(location[0], location[1], location[0]+piece_size[0], location[1])]
 
         self.piece_size = piece_size
         self.image_object = image_object
+        self.location = location
         if initiating_piece is not None:
-            connected_pieces.append(initiating_piece)
+            self.connected_pieces.append(initiating_piece)
 
-        # Any piece created AFTER the original will initialize with a jagged line
+        # Any piece created AFTER the original piece will initialize with a jagged line
         if incoming_side is not None:
             if incoming_side == "left":
                 self.right_side = side_data
@@ -40,89 +46,101 @@ class Piece:
     def add_side(self, side):
         # Create a new jagged puzzle line for the side
         if side == "left":
-            self.left_side = self.create_random_line(Piece.VERTICAL)
+            self.left_side = self.create_random_line_coordinates((self.left_side[0][0], self.left_side[0][1]), self.piece_size, Piece.LEFT)
             new_side_data = self.left_side
         elif side == "right":
-            self.right_side = self.create_random_line(Piece.VERTICAL)
+            self.right_side = self.create_random_line_coordinates((self.right_side[0][0], self.right_side[0][1]), self.piece_size, Piece.RIGHT)
             new_side_data = self.right_side
         elif side == "bottom":
-            self.bottom_side = self.create_random_line(Piece.HORIZONTAL)
+            self.bottom_side = self.create_random_line_coordinates((self.bottom_side[0][0], self.bottom_side[0][1]), self.piece_size, Piece.BOTTOM)
             new_side_data = self.bottom_side
         elif side == "top":
-            self.top_side = self.create_random_line(Piece.HORIZONTAL)
+            self.top_side = self.create_random_line_coordinates((self.top_side[0][0], self.top_side[0][1]), self.piece_size, Piece.TOP)
             new_side_data = self.top_side
         else:
             raise Exception()
 
         # Initialize a new puzzle piece
-        # self.connected_pieces.append(new Piece(self, side, new_side_data))
+        new_piece_location = (self.location[0], self.location[1]+self.piece_size[0]+50)
+        self.connected_pieces.append(Piece(new_piece_location, self.piece_size, self, side, new_side_data, self.image_object))
 
-    def create_random_line(last_location, orientation):
-        # Draw random line based on this pieces current location
-        if orientation == Piece.HORIZONTAL:
-            angle = 0
-        elif orientation == Piece.VERTICAL:
-            angle = 90
+    def create_random_line_coordinates(self, starting_location, size, side):
+        new_lines = []
+
+        arc_increment_size = 0.5
+        diameter = (arc_increment_size * 180) / math.pi
+
+        # Draw random line based on this piece's current location
+        if side == Piece.TOP:
+            line_angle = 0
+            arc_starting_angle = 270
+            line_size = (size[1]-diameter)/2
+        elif side == Piece.BOTTOM:
+            line_angle = 0
+            arc_starting_angle = 90
+            line_size = (size[1]-diameter)/2
+        elif side == Piece.RIGHT:
+            line_angle = 0
+            arc_starting_angle = 0
+            line_size = (size[1]-diameter)/2
+        elif side == Piece.LEFT:
+            line_angle = 90
+            arc_starting_angle = 180
+            line_size = (size[0]-diameter)/2
         else:
             raise Exception()
 
         # Draw line
-        last_location = draw_line(starting_location, 20, 90)
+        last_line = self.create_line_coordinates(starting_location, line_size, line_angle)
+        new_lines.append(last_line)
 
-        # Draw an arc
-        for angle in range(0, 92, 2):
-            last_location = draw_line(last_location, 1, angle)
-        for angle in range(90, 182, 2):
-            last_location = draw_line(last_location, 1, angle)
+        # Draw arc; LEFT and BOTTOM sides will draw in opposite directions than RIGHT and TOP
+        if side == Piece.BOTTOM or side == Piece.LEFT:
+            # Draw an arc
+            for a in range(arc_starting_angle, (arc_starting_angle-90) % 360, -2):
+                last_line = self.create_line_coordinates(last_line, 0.5, a)
+                new_lines.append(last_line)
+            for a in range((arc_starting_angle-91) % 360, (arc_starting_angle-182) % 360, -2):
+                last_line = self.create_line_coordinates(last_line, 0.5, a)
+                new_lines.append(last_line)
+        else:
+            # Draw an arc
+            for a in range(arc_starting_angle, (arc_starting_angle+92) % 360, 2):
+                last_line = self.create_line_coordinates(last_line, 0.5, a)
+                new_lines.append(last_line)
+            for a in range((arc_starting_angle+90) % 360, (arc_starting_angle+182) % 360, 2):
+                last_line = self.create_line_coordinates(last_line, 0.5, a)
+                new_lines.append(last_line)
 
         # Draw Line
-        last_location = draw_line(last_location, 20, 90)
+        last_line = self.create_line_coordinates(last_line, line_size, line_angle)
+        new_lines.append(last_line)
 
-        return last_location
+        return new_lines
+
+    def create_line_coordinates(self, start, size, direction):
+        # Determine if the starting location is a line or simple x, y coordinates
+        if len(start) == 2:  # simple x, y coordinates
+            start_x = start[0]
+            start_y = start[1]
+        elif len(start) == 4:  # contains line; x1, y1, x2, y2
+            start_x = start[2]
+            start_y = start[3]
+        else:
+            raise Exception("param: start contains incorrect number of tuple values.")
+
+        # Calculate X, Y coordinates based on direction and size
+        opposite = math.sin(math.radians(direction)) * size
+        adjacent = math.cos(math.radians(direction)) * size
+
+        # Return coordinates for the newly created line; x1, y1, x2, y2
+        return (start_x, start_y, start_x+adjacent, start_y+opposite)
 
     def draw(self):
         draw = ImageDraw.Draw(self.image_object)
         for side in [self.left_side, self.right_side, self.top_side, self.bottom_side]:
-            draw.line(side, fill=(0,0,0,0), width=2)
-
-
-# line(x1, y1, x2, y2)
-def draw_triangle_bump(draw_obj, start, size, direction):
-    # Calculate
-
-    """Draws a symmetric triangle-like bump on a vertical line."""
-    draw_obj.line((start[0], start[1], start[0]+size[0], start[1]+size[1]), fill=(0, 0, 0), width=1)
-    draw_obj.line((start[0]+size[0], start[1]+size[1], start[0], start[1]+(2*size[1])), fill=(0, 0, 0), width=1)
-
-    return (start[0], start[1]+(2*size[1]))
-
-
-def draw_line(start, random, size, direction, image_object):
-    # Init lines to draw
-    draw = ImageDraw.Draw(image_object)
-
-    # Calculate X, Y coordinates based on direction and size
-    opposite = math.sin(math.radians(direction)) * size
-    adjacent = math.cos(math.radians(direction)) * size
-
-    # Draw the line using coordinates
-    draw.line((start[0], start[1], start[0]+adjacent, start[1]+opposite), fill=(0, 0, 0), width=1)
-
-    return (start[0]+adjacent, start[1]+opposite)
-
-
-def draw_puzzle_piece(starting_location, image_object):
-    # Draw top line
-    last_location = draw_line(starting_location, 100, 0, image_object)
-
-    # Draw matching side
-    last_location = draw_puzzle_side(last_location, image_object)
-
-    # Draw bottom and left side
-    last_location = draw_line(last_location, 100, 180, image_object)
-    last_location = draw_line(last_location, 100, 270, image_object)
-
-    return last_location
+            for line in side:
+                draw.line(line, fill=(0,0,0,0), width=2)
 
 
 def main():
@@ -130,6 +148,7 @@ def main():
     im = Image.new('RGB', (500, 300), (255, 255, 255))
 
     first_piece = Piece((100, 100), (50, 50), None, None, None, im)
+    first_piece.add_side("bottom")
     first_piece.draw()
 
     # Save the image
